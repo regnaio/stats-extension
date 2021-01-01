8e56fc5f5389593d424fd9cbc3ce4066d8dbb323
regnaio <gbzregna@gmail.com>
2021-01-01 00:45:03 -0800

    message

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,17 @@
-function toggle(div) {
-  if (div.style.display === 'none') {
-    div.style.display = 'block';
-  } else {
-    div.style.display = 'none';
-  }
-}
+// function toggle(div) {
+//   if (div.style.display === 'none') {
+//     div.style.display = 'block';
+//   } else {
+//     div.style.display = 'none';
+//   }
+// }
 
 chrome.browserAction.onClicked.addListener(tab => {
-  alert('Toggle');
-  
-  const fpsDiv = document.body.getElementById('fps');
-  const frameRenderDurationDiv = document.body.getElementById('frameRenderDuration');
-  const memoryDiv = document.body.getElementById('memory');
-
-  toggle(fpsDiv);
-  toggle(frameRenderDurationDiv);
-  toggle(memoryDiv);
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.tabs.sendMessage(tabs[0].id, { text: 'toggle' }, response => {
+      if (response.type == 'answer') {
+        console.log('Answer');
+      }
+    });
+  });
 });
--- extension/content.js
+++ extension/content.js
@@ -14,3 +14,31 @@ const injectScript = function (scriptUrl) {
 injectScript('stats.min.js');
 injectScript('dat.gui.min.js');
 injectScript('overlay.js');
+
+function toggleDiv(div) {
+  if (div.style.display === 'none') {
+    div.style.display = 'block';
+  } else {
+    div.style.display = 'none';
+  }
+}
+
+function toggle() {
+  const fpsDiv = document.getElementById('fps');
+  const frameRenderDurationDiv = document.getElementById('frameRenderDuration');
+  const memoryDiv = document.getElementById('memory');
+
+  toggleDiv(fpsDiv);
+  toggleDiv(frameRenderDurationDiv);
+  toggleDiv(memoryDiv);
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.text == 'toggle') {
+    console.log('Toggle');
+    toggle();
+    sendResponse({
+      type: 'answer'
+    });
+  }
+});
