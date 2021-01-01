cc6847c33b01a691aea3356fbbed6385c408cca3
regnaio <gbzregna@gmail.com>
2021-01-01 00:46:27 -0800

    delete

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,3 @@
-// function toggle(div) {
-//   if (div.style.display === 'none') {
-//     div.style.display = 'block';
-//   } else {
-//     div.style.display = 'none';
-//   }
-// }
-
 chrome.browserAction.onClicked.addListener(tab => {
   chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.sendMessage(tabs[0].id, { text: 'toggle' }, response => {
